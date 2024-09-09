ffa7a7371660e932b7027056f7ba4c540e6f4020
YheChen <yanzhenchen123@gmail.com>
2024-09-09 12:03:33 -0400

    added a fizzbuzz call

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main();
+    }
 }
